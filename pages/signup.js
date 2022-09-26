6fa909f02527d53770a330a2516b56996c158c06
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-09-26 15:45:38 +0530

    Add

--- pages/signup.js
+++ pages/signup.js
@@ -42,27 +42,27 @@ const SignUp = () => {
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"
       ></script>
-         <div class="container">
-    <div class="row">
+         <div class="container" >
+    <div class="row ">
       <div class="col-md-6 offset-md-3">
         <h2 class="text-center text-dark mt-5">Signup Form</h2>
-        <div class="card my-5">
+        <div class="card my-5 ">
 
           <form class="card-body cardbody-color p-lg-5">
 
             <div class="text-center">
-              <img src="../logo.png.png"  class="img-fluid profile-image-pic  rounded-circle my-3"
+              <img src="../logo.png.png"  class="img-fluid profile-image-pic  rounded-circle my-3 rounded-sm"
                 width="200px" alt="profile"/>
             </div>
 
-            <div class="mb-3">
-              <input type="text" class="form-control" id="Username" aria-describedby="emailHelp"
-                placeholder="User Name"/>
+            <div class="mb-3 ">
+              <input type="text" class="form-control " id="Username" aria-describedby="emailHelp"
+                placeholder="UserName"/>
             </div>
             <div class="mb-3">
               <input type="password" class="form-control" id="password" placeholder="password"/>
             </div>
-            <div class="text-center"><button type="submit" class="btn btn-dark px-5 mb-5 w-100">Signup</button></div>
+            <div class="text-center"><button type="submit" class="btn btn-dark px-5 mb-5 w-100">Submit</button></div>
             <div id="emailHelp" class="form-text text-center mb-5 text-dark">Already Signup <a href="#" class="text-dark fw-bold"> Login</a>
             </div>
           </form>
